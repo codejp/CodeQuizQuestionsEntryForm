5218d7cac0d03b2124a380512892298681c7e96f
jsakamoto <junichi.jp@gmail.com>
2013-07-08 21:24:27 +0900

    外部ログインだけを使うようにして、コードを整理・削減。

--- CodeQuizQuestionsEntryForm/Controllers/AccountController.cs
+++ CodeQuizQuestionsEntryForm/Controllers/AccountController.cs
@@ -1,14 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Data.Entity.Validation;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using CodeQuizQuestionsEntryForm.Models;
 using Microsoft.AspNet.Identity;
 using Microsoft.Owin.Security;
-using CodeQuizQuestionsEntryForm.Models;
 
 namespace CodeQuizQuestionsEntryForm.Controllers
 {
@@ -40,171 +38,6 @@ namespace CodeQuizQuestionsEntryForm.Controllers
         }
 
         //
-        // POST: /Account/Login
-        [HttpPost]
-        [AllowAnonymous]
-        [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Login(LoginViewModel model, string returnUrl)
-        {
-            if (ModelState.IsValid)
-            {
-                // Validate the user password
-                if (await Secrets.Validate(model.UserName, model.Password))
-                {
-                    string userId = await Logins.GetUserId(IdentityConfig.LocalLoginProvider, model.UserName);
-                    await SignIn(userId, model.RememberMe);
-                    return RedirectToLocal(returnUrl);
-                }
-            }
-
-            // If we got this far, something failed, redisplay form
-            ModelState.AddModelError(String.Empty, "The user name or password provided is incorrect.");
-            return View(model);
-        }
-
-        //
-        // GET: /Account/Register
-        [AllowAnonymous]
-        public ActionResult Register()
-        {
-            return View();
-        }
-
-        //
-        // POST: /Account/Register
-        [HttpPost]
-        [AllowAnonymous]
-        [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Register(RegisterViewModel model)
-        {
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    // Create a profile, password, and link the local login before signing in the user
-                    User user = new User(model.UserName);
-                    if (await Users.Create(user) &&
-                        await Secrets.Create(new UserSecret(model.UserName, model.Password)) &&
-                        await Logins.Add(new UserLogin(user.Id, IdentityConfig.LocalLoginProvider, model.UserName)))
-                    {
-                        await SignIn(user.Id, isPersistent: false);
-                        return RedirectToAction("Index", "Home");
-                    }
-                    else
-                    {
-                        ModelState.AddModelError(String.Empty, "Failed to create login for: " + model.UserName);
-                    }
-                }
-                catch (DbEntityValidationException e)
-                {
-                    ModelState.AddModelError("", e.EntityValidationErrors.First().ValidationErrors.First().ErrorMessage);
-                }
-            }
-
-            // If we got this far, something failed, redisplay form
-            return View(model);
-        }
-
-        //
-        // POST: /Account/Disassociate
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Disassociate(string loginProvider, string providerKey)
-        {
-            ManageMessageId? message = null;
-            string userId = User.Identity.GetUserId();
-            if (await UnlinkAccountForUser(userId, loginProvider, providerKey))
-            {
-                // If you remove a local login, need to delete the login as well
-                if (loginProvider == IdentityConfig.LocalLoginProvider)
-                {
-                    await Secrets.Delete(providerKey);
-                }
-                message = ManageMessageId.RemoveLoginSuccess;
-            }
-
-            return RedirectToAction("Manage", new { Message = message });
-        }
-
-        //
-        // GET: /Account/Manage
-        public async Task<ActionResult> Manage(ManageMessageId? message)
-        {
-            ViewBag.StatusMessage =
-                message == ManageMessageId.ChangePasswordSuccess ? "Your password has been changed."
-                : message == ManageMessageId.SetPasswordSuccess ? "Your password has been set."
-                : message == ManageMessageId.RemoveLoginSuccess ? "The external login was removed."
-                : String.Empty;
-            string localUserName = await Logins.GetProviderKey(User.Identity.GetUserId(), IdentityConfig.LocalLoginProvider);
-            ViewBag.UserName = localUserName;
-            ViewBag.HasLocalPassword = localUserName != null;
-            ViewBag.ReturnUrl = Url.Action("Manage");
-            return View();
-        }
-
-        //
-        // POST: /Account/Manage
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Manage(ManageUserViewModel model)
-        {
-            string userId = User.Identity.GetUserId();
-            string localUserName = await Logins.GetProviderKey(User.Identity.GetUserId(), IdentityConfig.LocalLoginProvider);
-            bool hasLocalLogin = localUserName != null;
-            ViewBag.HasLocalPassword = hasLocalLogin;
-            ViewBag.ReturnUrl = Url.Action("Manage");
-            if (hasLocalLogin)
-            {               
-                if (ModelState.IsValid)
-                {
-                    bool changePasswordSucceeded = await ChangePassword(localUserName, model.OldPassword, model.NewPassword);
-                    if (changePasswordSucceeded)
-                    {
-                        return RedirectToAction("Manage", new { Message = ManageMessageId.ChangePasswordSuccess });
-                    }
-                    else
-                    {
-                        ModelState.AddModelError(String.Empty, "The current password is incorrect or the new password is invalid.");
-                    }
-                }
-            }
-            else
-            {
-                // User does not have a local password so remove any validation errors caused by a missing OldPassword field
-                ModelState state = ModelState["OldPassword"];
-                if (state != null)
-                {
-                    state.Errors.Clear();
-                }
-
-                if (ModelState.IsValid)
-                {
-                    try
-                    {
-                        // Create the local login info and link the local account to the user
-                        localUserName = User.Identity.GetUserName();
-                        if (await Secrets.Create(new UserSecret(localUserName, model.NewPassword)) &&
-                            await Logins.Add(new UserLogin(userId, IdentityConfig.LocalLoginProvider, localUserName)))
-                        {
-                            return RedirectToAction("Manage", new { Message = ManageMessageId.SetPasswordSuccess });
-                        }
-                        else
-                        {
-                            ModelState.AddModelError(String.Empty, "Failed to set password");
-                        }
-                    }
-                    catch (Exception e)
-                    {
-                        ModelState.AddModelError(String.Empty, e);
-                    }
-                }
-            }
-
-            // If we got this far, something failed, redisplay form
-            return View(model);
-        }
-
-        //
         // POST: /Account/ExternalLogin
         [HttpPost]
         [AllowAnonymous]
@@ -221,21 +54,22 @@ namespace CodeQuizQuestionsEntryForm.Controllers
         public async Task<ActionResult> ExternalLoginCallback(string loginProvider, string returnUrl)
         {
             // Get the information about the user from the external login provider
-            ClaimsIdentity id = await HttpContext.GetExternalIdentity();
+            var id = await HttpContext.GetExternalIdentity();
             if (id == null)
             {
                 return View("ExternalLoginFailure");
             }
 
             // Make sure the external identity is from the loginProvider we expect
-            Claim providerKeyClaim = id.FindFirst(ClaimTypes.NameIdentifier);
-            if (providerKeyClaim == null || providerKeyClaim.Issuer != loginProvider) {
+            var providerKeyClaim = id.FindFirst(ClaimTypes.NameIdentifier);
+            if (providerKeyClaim == null || providerKeyClaim.Issuer != loginProvider)
+            {
                 return View("ExternalLoginFailure");
             }
 
             // Succeeded so we should be able to lookup the local user name and sign them in
-            string providerKey = providerKeyClaim.Value;
-            string userId = await Logins.GetUserId(loginProvider, providerKey);
+            var providerKey = providerKeyClaim.Value;
+            var userId = await Logins.GetUserId(loginProvider, providerKey);
             if (!String.IsNullOrEmpty(userId))
             {
                 await SignIn(userId, id.Claims, isPersistent: false);
@@ -250,40 +84,10 @@ namespace CodeQuizQuestionsEntryForm.Controllers
                 }
                 else
                 {
-                    ViewBag.ReturnUrl = returnUrl;
-                    return View("ExternalLoginConfirmation", new ExternalLoginConfirmationViewModel { UserName = id.Name, LoginProvider = loginProvider });
-                }
-            }
-
-            return RedirectToLocal(returnUrl);
-        }
-
-        //
-        // POST: /Account/ExternalLoginConfirmation
-        [HttpPost]
-        [AllowAnonymous]
-        [ValidateAntiForgeryToken]
-        public async Task<ActionResult> ExternalLoginConfirmation(ExternalLoginConfirmationViewModel model, string returnUrl)
-        {
-            if (User.Identity.IsAuthenticated)
-            {
-                return RedirectToAction("Manage");
-            }
-            
-            if (ModelState.IsValid)
-            {
-                // Get the information about the user from the external login provider
-                ClaimsIdentity id = await HttpContext.GetExternalIdentity();
-                if (id == null)
-                {
-                    return View("ExternalLoginFailure");
-                }
-                try
-                {
                     // Create a local user and sign in
-                    var user = new User(model.UserName);
+                    var user = new User(id.Name);
                     if (await Users.Create(user) &&
-                        await Logins.Add(new UserLogin(user.Id, model.LoginProvider, id.FindFirstValue(ClaimTypes.NameIdentifier))))
+                        await Logins.Add(new UserLogin(user.Id, loginProvider, id.FindFirstValue(ClaimTypes.NameIdentifier))))
                     {
                         await SignIn(user.Id, id.Claims, isPersistent: false);
                         return RedirectToLocal(returnUrl);
@@ -293,14 +97,9 @@ namespace CodeQuizQuestionsEntryForm.Controllers
                         return View("ExternalLoginFailure");
                     }
                 }
-                catch (DbEntityValidationException e)
-                {
-                    ModelState.AddModelError("", e.EntityValidationErrors.First().ValidationErrors.First().ErrorMessage);
-                }
             }
 
-            ViewBag.ReturnUrl = returnUrl;
-            return View(model);
+            return RedirectToLocal(returnUrl);
         }
 
         //
@@ -329,18 +128,6 @@ namespace CodeQuizQuestionsEntryForm.Controllers
             return (ActionResult)PartialView("_ExternalLoginsListPartial", new List<AuthenticationDescription>(HttpContext.GetExternalAuthenticationTypes()));
         }
 
-
-        [ChildActionOnly]
-        public ActionResult RemoveAccountList()
-        {
-            return Task.Run(async () =>
-            {
-                var linkedAccounts = await Logins.GetLogins(User.Identity.GetUserId());
-                ViewBag.ShowRemoveButton = linkedAccounts.Count > 1;
-                return (ActionResult)PartialView("_RemoveAccountPartial", linkedAccounts);
-            }).Result;
-        }
-       
         #region Helpers
         private ActionResult RedirectToLocal(string returnUrl)
         {
@@ -352,30 +139,6 @@ namespace CodeQuizQuestionsEntryForm.Controllers
             {
                 return RedirectToAction("Index", "Home");
             }
-        }
-
-        private async Task<bool> UnlinkAccountForUser(string userId, string loginProvider, string providerKey)
-        {
-            string ownerAccount = await Logins.GetUserId(loginProvider, providerKey);
-            if (ownerAccount == userId)
-            {
-                if ((await Logins.GetLogins(userId)).Count > 1)
-                {
-                    await Logins.Remove(userId, loginProvider, providerKey);
-                    return true;
-                }
-            }
-            return false;
-        }
-
-        private async Task<bool> ChangePassword(string userName, string oldPassword, string newPassword)
-        {
-            bool changePasswordSucceeded = false;
-            if (await Secrets.Validate(userName, oldPassword))
-            {
-                changePasswordSucceeded = await Secrets.UpdateSecret(userName, newPassword);
-            }
-            return changePasswordSucceeded;
         }
 
         private Task SignIn(string userId, bool isPersistent)
@@ -412,14 +175,7 @@ namespace CodeQuizQuestionsEntryForm.Controllers
                 context.HttpContext.Challenge(LoginProvider, new AuthenticationExtra() { RedirectUrl = RedirectUrl });
             }
         }
-        
-        public enum ManageMessageId
-        {
-            ChangePasswordSuccess,
-            SetPasswordSuccess,
-            RemoveLoginSuccess,
-        }
-        
+
         #endregion
     }
 }
