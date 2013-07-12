027195333efec6762603c772b9d63ab66b9e8eb9
jsakamoto <junichi.jp@gmail.com>
2013-07-12 20:28:38 +0900

    画像の添付に対応

--- CodeQuizQuestionsEntryForm/Global.asax.cs
+++ CodeQuizQuestionsEntryForm/Global.asax.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using CodeQuizQuestionsEntryForm.Models;
 
 namespace CodeQuizQuestionsEntryForm
 {
@@ -22,6 +24,8 @@ namespace CodeQuizQuestionsEntryForm
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            Database.SetInitializer(new DropCreateDatabaseIfModelChanges<IdentityDbContext>());
         }
     }
 }
--- CodeQuizQuestionsEntryForm/Models/Question.cs
+++ CodeQuizQuestionsEntryForm/Models/Question.cs
@@ -21,27 +21,48 @@ namespace CodeQuizQuestionsEntryForm.Models
 
         [Display(Name = "回答選択肢1"), Required, AllowHtml]
         public string Option1 { get; set; }
+        [Display(Name = "回答選択肢画像1")]
+        public string OptionImage1 { get; set; }
+
         [Display(Name = "回答選択肢2"), Required, AllowHtml]
         public string Option2 { get; set; }
+        [Display(Name = "回答選択肢画像2")]
+        public string OptionImage2 { get; set; }
+
         [Display(Name = "回答選択肢3"), AllowHtml]
         public string Option3 { get; set; }
+        [Display(Name = "回答選択肢画像3")]
+        public string OptionImage3 { get; set; }
+        
         [Display(Name = "回答選択肢4"), AllowHtml]
         public string Option4 { get; set; }
+        [Display(Name = "回答選択肢画像4")]
+        public string OptionImage4 { get; set; }
+
         [Display(Name = "回答選択肢5"), AllowHtml]
         public string Option5 { get; set; }
+        [Display(Name = "回答選択肢画像5")]
+        public string OptionImage5 { get; set; }
+
         [Display(Name = "回答選択肢6"), AllowHtml]
         public string Option6 { get; set; }
+        [Display(Name = "回答選択肢画像6")]
+        public string OptionImage6 { get; set; }
 
-        public string[] GetOptions()
+        public OptionViewModel[] GetOptions(bool trim = true)
         {
+            Func<OptionViewModel, bool> filter = trim ?
+                (Func<OptionViewModel, bool>)(opt => string.IsNullOrEmpty(opt.Option) == false) :
+                (Func<OptionViewModel, bool>)(_ => true);
+
             return new[] { 
-                Option1,
-                Option2,
-                Option3,
-                Option4,
-                Option5,
-                Option6
-            }.Where(s => string.IsNullOrEmpty(s) == false).ToArray();
+                new OptionViewModel(1, Option1, OptionImage1),
+                new OptionViewModel(2, Option2, OptionImage2),
+                new OptionViewModel(3, Option3, OptionImage3),
+                new OptionViewModel(4, Option4, OptionImage4),
+                new OptionViewModel(5, Option5, OptionImage5),
+                new OptionViewModel(6, Option6, OptionImage6)
+            }.Where(filter).ToArray();
         }
 
         [Display(Name = "正解の選択肢の番号")]
